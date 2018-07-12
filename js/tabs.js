45f69086b7af71e8870ed279b7f514b076aca135
Your Name <email@example.com>
2018-07-12 22:48:42 +0800

    切换功能实现

--- js/tabs.js
+++ js/tabs.js
@@ -28,25 +28,46 @@
 
       if(config.triggerType==="click") {
       	this.tabItems.bind(config.triggerType, function() {
-      		alert(1);
+      		_this_.invoke($(this));
       	})
       } else if(config.triggerType==="mouseover" || config.triggerType!="click") {
       	this.tabItems.mouseover(function() {
-      		alert(2);
+      		_this_.invoke($(this));
       	})
       }
 	};
 
 	Tab.prototype = {  
+	  // 事件驱动函数
+	  invoke: function(currentTab) {
+	  	var _this_ = this;
+
+	  	/**
+        *要执行Tab的选中状态,当前选中的加上actived(标记为白底) 
+	  	*切换对应的tab内容,要根据配置参数的effect是default还是fade
+	  	*/
+	  	var index = currentTab.index();
+	  	// tab选中状态
+	  	currentTab.addClass("actived").siblings().removeClass("actived");
+	  	//切换对应的内容区域
+	  	var effect = this.config.effect;
+	  	var conItems = this.contentItems;
+
+	  	if(effect === "default" || effect!= "fade") {
+           conItems.eq(index).addClass("current").siblings().removeClass("current");
+	  	} else if(effect === "fade") {
+           conItems.eq(index).fadeIn().siblings().fadeOut();
+	  	}
+	  },
       // 获取配置参数
-      getConfig: function() {
+      getConfig:function() {
       	// 拿一下tab elem节点的data-config
       	var config = this.tab.attr("data-config");
       	// 确保有配置参数
       	if(config&&config!="") {
       		return $.parseJSON(config);
       	} else {
-      		return null;
+      		return null
       	}
       }
 	};
